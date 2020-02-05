3059ac1e3229d45824101b968351707b89479d5f
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-02-05 21:14:41 +0700

    Update firebase.js

---
+++ build/login/js/firebase.js
@@ -0,0 +1,20 @@
+// The core Firebase JS SDK is always required and must be listed first
+src = "https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"
+
+//TODO: Add SDKs for Firebase products that you want to use
+// https: //firebase.google.com/docs/web/setup#available-libraries 
+src = "https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBOJY8Sdddvin3x4edZBjHI_3IaOYMGcCc",
+    authDomain: "app-demo-231d0.firebaseapp.com",
+    databaseURL: "https://app-demo-231d0.firebaseio.com",
+    projectId: "app-demo-231d0",
+    storageBucket: "app-demo-231d0.appspot.com",
+    messagingSenderId: "603311182490",
+    appId: "1:603311182490:web:4fa79bd98ac84f9a21c523",
+    measurementId: "G-TTYKDSJMMY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
