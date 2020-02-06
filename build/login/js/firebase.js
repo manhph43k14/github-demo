1b7d1aa5e40dd7677997fa2ca38d9e112f8f2655
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-02-06 12:51:36 +0700

    Update firebase.js

--- build/login/js/firebase.js
+++ build/login/js/firebase.js
@@ -1,20 +1,17 @@
-// The core Firebase JS SDK is always required and must be listed first
-src = "https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"
+var app_firebase = {};
+(function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyBOJY8Sdddvin3x4edZBjHI_3IaOYMGcCc",
+        authDomain: "app-demo-231d0.firebaseapp.com",
+        databaseURL: "https://app-demo-231d0.firebaseio.com",
+        projectId: "app-demo-231d0",
+        storageBucket: "app-demo-231d0.appspot.com",
+        messagingSenderId: "603311182490",
+        appId: "1:603311182490:web:4fa79bd98ac84f9a21c523",
+        measurementId: "G-TTYKDSJMMY"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-//TODO: Add SDKs for Firebase products that you want to use
-// https: //firebase.google.com/docs/web/setup#available-libraries 
-src = "https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"
-
-var firebaseConfig = {
-    apiKey: "AIzaSyBOJY8Sdddvin3x4edZBjHI_3IaOYMGcCc",
-    authDomain: "app-demo-231d0.firebaseapp.com",
-    databaseURL: "https://app-demo-231d0.firebaseio.com",
-    projectId: "app-demo-231d0",
-    storageBucket: "app-demo-231d0.appspot.com",
-    messagingSenderId: "603311182490",
-    appId: "1:603311182490:web:4fa79bd98ac84f9a21c523",
-    measurementId: "G-TTYKDSJMMY"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    app_firebase = firebase;
+})()
