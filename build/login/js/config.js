e4cde5d221b0f5134238911d97dbb3176ce919a2
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-18 17:05:40 +0700

    Delete config.js

--- build/login/js/config.js
+++
@@ -1,3 +0,0 @@
-// Import Boostrap css, js, font awesome here -->
-src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
-src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
